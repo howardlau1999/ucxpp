25ba92d470e3b76f599fe7dbd2032deefedb304b
Howard Lau <howardlau1999@hotmail.com>
2022-08-23 08:32:41 +0000

    remote_address: add copy and move ctor and asgn

--- include/ucxpp/address.h
+++ include/ucxpp/address.h
@@ -95,9 +95,39 @@ public:
   /**
    * @brief Construct a new remote address object
    *
-   * @param address Anothr remote address object to move from
+   * @param address Another remote address object to move from
    */
   remote_address(std::vector<char> &&address);
+
+  /**
+   * @brief Move construct a new remote address object
+   *
+   * @param other Another remote address object to move from
+   */
+  remote_address(remote_address &&other) = default;
+
+  /**
+   * @brief Construct a new remote address object
+   *
+   * @param other Another remote address object to copy from
+   */
+  remote_address(remote_address const &other) = default;
+
+  /**
+   * @brief Copy assignment operator
+   *
+   * @param other Another remote address object to copy from
+   * @return remote_address& This object
+   */
+  remote_address &operator=(remote_address const &other) = default;
+
+  /**
+   * @brief Move assignment operator
+   *
+   * @param other Another remote address object to move from
+   * @return remote_address& This object
+   */
+  remote_address &operator=(remote_address &&other) = default;
 
   /**
    * @brief Get the UCP address
